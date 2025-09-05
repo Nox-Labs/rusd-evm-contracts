056dbeebeaa77718e792dee9e4b9bec412599951
Nox-admin <hello@noxlabs.xyz>
2025-09-05 11:32:18 +0300

    fix:CVF#20CVF#22,CVF#26

--- src/lib/RingBufferLib.sol
+++ src/lib/RingBufferLib.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 /**
  * NOTE: There is a difference in meaning between "cardinality" and "count":
